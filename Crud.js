7c30d2ea7cf091d06269fa8d0a5807e9d858d980
Nursultan0402 <136288732+Nursultan0402@users.noreply.github.com>
2023-07-18 16:39:07 +0500

    Add files via upload

---
+++ Crud.js
@@ -0,0 +1,44 @@
+
+// let users = [
+//     { id: 1, year: 1998, engine: 1, name: "Tico" },
+//     { id: 2, year: 2005, engine: 1.2, name: "Matiz" },
+//     { id: 3, year: 2010, engine: 1.6, name: "Gentra" }, { id: 4, year: 2010, engine: 1.5, name: "Cobalt" },
+//     { id: 5, year: 2012, engine: 2, name: "Malibu" }, { id: 6, year: 2000, engine: 1.2, name: "Damas" },
+//     { id: 7, year: 2018, engine: 2.4, name: "Tracker" },
+//     ];
+
+    // let filtered = users.filter(value => value.year <= 2000);
+    // console.log(filtered);
+
+    // let filtered = users.filter(value => value.year > 2010);
+    // console.log(filtered);
+
+    // let filtered = users.filter(value => value.year < 2010);
+    // console.log(filtered);
+
+    // let sorted = users.sort((a, b) => a.engine - b.engine);
+    // console.log(sorted);
+
+    // let sorted = users.sort((a, b) => a.year - b.year);
+    // console.log(sorted);
+
+    // let sorted = users.sort((a, b) => a.name.localeCompare (b.name));
+    // console.log(sorted);
+
+    // let filtered = users.filter(value => value.year <= 2000);
+    // let mapped = filtered.map(value => {
+    //     return {...value, status: 'eski'}
+    // })
+    // console.log(mapped);
+
+    // let filtered = users.filter(value => value.year > 2000 && value.year < 2010);
+    // let mapped = filtered.map(value => {
+    //     return {...value, status: 'urta'}
+    // })
+    // console.log(mapped);
+
+    // let filtered = users.filter(value => value.year > 2010 && value.year < 2022);
+    // let mapped = filtered.map(value => {
+    //     return {...value, status: 'yangi'}
+    // })
+    // console.log(mapped);
